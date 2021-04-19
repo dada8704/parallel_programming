b973f6ca56862bc39221e2d67ef3be314af12514
Dada870423 <dada0423.cs05@g2.nctu.edu.tw>
2021-04-19 12:06:07 +0000

    init lab3

---
+++ Lab3_SSW/Fasta.hpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<string>
+
+class Fasta
+{
+
+private:
+    int CppIsGood;
+    std::string seq;
+
+public:
+    Fasta(std::string input) : CppIsGood(0), seq(input) {  }
+    Fasta() : CppIsGood(0), seq("NNNNNN") {  }
+};
---
+++ Lab3_SSW/ReadFasta.hpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<string>
+#include<fstream>
+
+#include "Fasta.hpp"
+
+class ReadFasta
+{
+private:
+    int fuck;
+public:
+    ReadFasta() : fuck(0) { }
+    static Fasta  read(std::string filename) 
+    { 
+        std::cout << "reading " << filename << "\n";
+        std::ifstream istrm(filename);
+        if(!istrm.is_open())
+        {
+            std::cout << "fail to open " << filename << "\n";
+            exit(0);
+        }
+        
+        
+        std::string trash, seq;
+        if(std::getline(istrm, trash))
+            std::cout << trash << "\n";
+        if(std::getline(istrm, seq))
+            std::cout << seq << "\n";
+        
+
+        return Fasta(seq);  
+    }
+};
