9c3adfe3aedd9fc3c739ab7ffc892c402574c3aa
Dada870423 <dada0423.cs05@g2.nctu.edu.tw>
2021-04-27 10:20:10 +0000

    SSW & SW done

--- Lab3_SSW/SmithWaterman.hpp
+++ Lab3_SSW/SmithWaterman.hpp
@@ -10,29 +10,54 @@
 class SmithWaterman
 {
 private:
-    constexpr static int mismatch = 2, indel = 1, match = 2;
+    constexpr static int mismatch = 3, indel = 2, match = 3;
 public:
     SmithWaterman() { }
     
-    static int trace(std::vector<std::vector<int>>& dp, std::pair<int, int> end_point)
+    static std::pair<int, std::vector<int>> trace(std::vector<std::vector<int>>& dp, std::pair<int, int> end_point, std::string& ref, std::string& seq)
     {
         int i = end_point.first, j = end_point.second;
+        std::vector<int> state;
         std::cout << "i: " << i << " j: " << j << "\n";
         
         for(int j = end_point.second; j > -1;)
         {
             //std::cout << "dp: " << dp[i][j] << " i: " << i << " j: " << j << "\n";
-            if(dp[i][j] == 0 || j == 0) 
-                break;
-            else if(dp[i-1][j-1] + match == dp[i][j])//  && dp[i-1][j-1] >= dp[i][j-1])
+            //if(dp[i][j] == 0 || j == 0)
+            //{
+            //    std::cout << "dp: " << dp[i][j] << " j: " << j << "\n";
+            //    break;
+            //}
+            if((dp[i-1][j-1] + match == dp[i][j]) && ref[i-1] == seq[j-1])//  && dp[i-1][j-1] >= dp[i][j-1])
+            {
                 --i, --j;
-            else if(dp[i-1][j] >= dp[i][j-1])
+                if(j==1)
+                    std::cout << ref[i] << " " << seq[j-1] << seq[j] << " " << dp[i][j] << " " << dp[i-1][j-1] << "\n";
+                state.emplace_back(0);// matech
+            }
+            else if(dp[i-1][j] - indel == dp[i][j])
+            {
                 --i;
-            else
+                state.emplace_back(1); // delete
+            }
+            else if(dp[i][j-1] - indel == dp[i][j])
+            {
                 --j;
+                state.emplace_back(2);// insert
+            }
+            else if(dp[i-1][j-1] - mismatch == dp[i][j])// mismatch
+            {
+                --i, --j;
+                state.emplace_back(3);
+            }
+            else if(dp[i][j] == 0 || j == 0)
+            {
+                std::cout << "dp: " << dp[i][j] << " j: " << j << "\n";
+                break;
+            }
         }
         
-        return i;
+        return std::make_pair(i, state);
     }
 
     static std::pair<int, std::pair<int, int>> SSW(Fasta Ref, Fasta Seq)
@@ -72,13 +97,50 @@ public:
                 }       
             }
         }
-        auto start = trace(dp, best.second);
-        std::cout << "\n" << start << "  ans:\n\n";
+        std::pair<int, std::vector<int>> state = trace(dp, best.second, ref, seq);
+        std::cout << "\n" << state.first << "  ans:\n\n";
 
-        for(int i = start; i < best.second.first + 1; ++i)
-            std::cout << ref[i];
+
+        Output(ref, seq, state);
+
+        //for(int i = 0; i < best.second.first + 1; ++i)
+        //    std::cout << ref[i];
         std::cout << "\n\n";
         return best;
+    }
+
+    static void Output(std::string& ref, std::string& query, std::pair<int, std::vector<int>>& state)
+    {
+        std::cout << "all long: " << state.second.size() << "\n";
+        int ref_iter = state.first;
+        int query_iter = 0;
+        for(int i = state.second.size() - 1; i > -1; --i)
+        {
+            if(state.second[i] == 2)
+                std::cout << "-";
+            else
+                std::cout << ref[ref_iter++];
+        }
+        std::cout << "\n";
+        for(int i = state.second.size() - 1; i > -1; --i)
+        {
+            if(state.second[i] == 0)
+                std::cout << "|";
+            else
+                std::cout << " ";
+        }
+        std::cout << "\n";
+        for(int i = state.second.size() - 1; i > -1; --i)
+        {
+            if(state.second[i] == 1)
+                std::cout << "-";
+            else
+                std::cout << query[query_iter++];
+        }
+        std::cout << "\n";
+        for(int i = state.second.size() - 1; i > -1; --i)
+            std::cout << state.second[i];
+        std::cout << "\n";
     }
     
     static std::pair<int, std::pair<int, int>> SW(Fasta Ref, Fasta Seq)
@@ -116,10 +178,11 @@ public:
 
         }
         
-        auto start = trace(dp, best.second);
+        std::pair<int, std::vector<int>> state = trace(dp, best.second, ref, seq);
 
-        std::cout << "\n" << start << "  ans:\n\n";
-        for(int i = start; i < best.second.first + 1; ++i)
+        Output(ref, seq, state);
+        std::cout << "\n" << state.first << "  ans:\n\n";
+        for(int i = state.first; i < best.second.first + 1; ++i)
             std::cout << ref[i] ;
         std::cout << "\n\n";        
         return best; //position (start, end)
